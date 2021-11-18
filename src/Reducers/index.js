fa1efde5d32c6f8b7172ac5213a273cbc40aa2a6
Muhammad Omair <muhammadomair@crystal-lite.net>
2021-11-18 05:56:09 -0800

    file structure implemented / redux implemneted

---
+++ src/Reducers/index.js
@@ -0,0 +1,8 @@
+// import {combineReducers} from 'redux'
+
+// -------These reducer files will be created in Reducers folder then will be imported here
+// import CounterReducer from './CounterReducer'
+// import SimpleTextReducer from './SimpleTextReducer'
+
+// Here all reducers will get combined
+// export default RootReducer({counter: CounterReducer, text: SimpleTextReducer})
