b38daea03c6c59d51993ae632ecc3f2963601529
shoaibarif668 <shoaib.techxcape@gmail.com>
2021-12-08 21:41:00 +0500

    google contacts fix

--- src/Reducers/giftNFT__contactData.js
+++ src/Reducers/giftNFT__contactData.js
@@ -3,7 +3,7 @@ let initialvalue = [];
 const GoogleContactData = (state = initialvalue,action) => {
     switch (action.type) {
         case "getGoogleContactData":
-            return [...state,...action.payload]; //We spread the last state because this is a pure reducer function meaning every time we call it, it will return a new state so ...state means it will not forget the old state if the function is called again
+            return [...action.payload]; //We spread the last state because this is a pure reducer function meaning every time we call it, it will return a new state so ...state means it will not forget the old state if the function is called again
 
         default:
             return state;
